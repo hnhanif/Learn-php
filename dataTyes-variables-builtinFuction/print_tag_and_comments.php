1646c33fc672d94fcdd7b78fb738aecac670c43f
Abu Hanif <hanifgp2500@gmail.com>
2024-02-24 04:19:55 +0600

    initialize and learn about print,echo,comment and little php thing

---
+++ dataTyes-variables-builtinFuction/print_tag_and_comments.php
@@ -0,0 +1,21 @@
+<?php
+echo "hello"; //display output as it's content
+print "Hey"; // display output as string
+print "<br>"; // can support html tag
+echo "<h1>" . "There you go" . "<h1/>"; // can support html tag
+echo "Hello", "abc", 123; //can display multiple data 
+echo "<br>";
+// print"a","b";//can't support multiple value
+// single line comment
+
+/* multi line comment
+    one
+    two
+    three
+    .....    
+*/
+
+/*
+if .php file has not html file then that case we don't need ending php tag (?>)
+    either case if html tag in our file then we have to use ending tag (?>)
+*/
