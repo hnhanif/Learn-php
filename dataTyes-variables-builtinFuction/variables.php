ce323f7cd9ec422bd64afaa20ea60a01abfe6858
Abu Hanif <hanifgp2500@gmail.com>
2024-02-24 04:41:00 +0600

    learnt variables

---
+++ dataTyes-variables-builtinFuction/variables.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Variable Rules
+ * --------------
+ * Prefixed with $ (Dollar sign)
+ * Must start with a letter or an underscore
+ * Can not start with a number
+ * Can only contain letters, numbers and underscores
+ * Are case sensitive ($Name and $name are not the same)
+ */
+
+/**
+ * Varaible Naming Convention:
+ * --------------------------
+ * unserscore: $first_name ✔
+ * camlecase: $firstName ✔
+ * pascalcase: $FirstName ✔(for class)
+ * lowercase: $firstname
+ */
+
+/**
+ * <?php ?> and <?=?> are same for just in html content
+ */
+
+$title = "Variables";
+$heading = "I'm learning variables";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
+</head>
+
+<body>
+    <h1><?= $heading ?></h1>
+</body>
+
+</html>
