4bb17600ed41e210125a039bcbe4a42308e316e9
Abu Hanif <hanifgp2500@gmail.com>
2024-02-25 02:44:42 +0600

    learnt associative array

---
+++ 02_arrays-iteration/03_associative_arrays.php
@@ -0,0 +1,22 @@
+<?php
+
+/**Associative array is like javascript object*/
+$user = [
+    "username" => "aliensarmy",
+    "email" => "aliensarmy2500@gmail.com",
+    "password" => "whoknows123",
+    "hobbies" => ["Watch movies", "play clash of clans", "Watch anime"],
+];
+echo "<pre>";
+var_dump($user);
+echo "</pre>";
+
+//get value of specific key, if specific index not found then it will give undefined key error
+var_dump($user["username"]);
+echo "<br>";
+var_dump($user['hobbies'][2]);
+
+//add element in associative array
+$user['cool'] = "awesome";
+echo "<br>";
+var_dump($user);
