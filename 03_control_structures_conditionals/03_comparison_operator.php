e59632c10ae20ad6a1626ce22c3ed8fcb350dd77
Abu Hanif <hanifgp2500@gmail.com>
2024-02-26 17:42:48 +0600

    learnt comparison and logical operation

---
+++ 03_control_structures_conditionals/03_comparison_operator.php
@@ -0,0 +1,17 @@
+<?php
+$x = 10;
+$y = 20;
+echo $x != $y; // not equal
+print "<br>";
+echo $x <> $y; // not equal
+print "<br>";
+var_dump($x < $y) . "<br>"; //less than
+print "<br>";
+var_dump($x > $y); //greater than
+var_dump($x !== $y); // not identical
+print("<br>");
+
+// && exclusive and operator
+// || exclusive or operator
+// ! Not operator
+// xor (true if mutiple condition are not same)
