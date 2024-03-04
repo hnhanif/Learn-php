844c4356229f4b2b9976d556e95fecc2bf8f47c1
Abu Hanif <hanifgp2500@gmail.com>
2024-03-04 23:22:33 +0600

    know about superglobal variables

---
+++ 06_superglobals/01_intro.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * PHP superglobals
+ * -----------------
+ * $_GLOBALS: References variable in the global scope
+ * $_SERVER: Contains info about the server such as hostname, file paths, etc
+ * $_GET: Variables passed to the script via query params and GET forms
+ * $_POST: Variables passed to the script via POST forms
+ * $_FILES: Items uploaded to the current script
+ * $_COOKIE: Variable passed to script via HTTP cookies
+ * $_SESSION: Session variables available to the script
+ * $_REQUEST: Contains the contents of $_GET, $_POST and $_COOKIE
+ * $_ENV: Variables passed to the current script via environment method
+ */
